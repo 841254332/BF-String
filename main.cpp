fa578600ce4105fc3c315926003d5f9d65d5817e
luojiahuan <841254332@qq.com>
2016-09-22 15:35:26 +0800

    BF

---
+++ main.cpp
@@ -0,0 +1,41 @@
+ //
+//  main.cpp
+//  BF-string
+//
+//  Created by luojiahua on 16/9/22.
+//  Copyright © 2016年 luojiahua. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int index(string S, string T){
+    int i = 0;
+    int j = 0;
+    while (i<=S.length()-1&&j<=T.length()-1) {
+        if(S[i] == T[j]){
+            i++;
+            j++;
+        }else{
+            i = i - j + 1;
+            j = 0;
+        }
+        cout<<i<<endl;
+        
+    }
+    if (j >= T.length()-1) {
+        cout<< (int)(i-T.length());
+    }else{
+        cout<<-1;
+    }
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string s = "abcde";
+    string t = "e";
+    index(s, t);
+    return 0;
+}
